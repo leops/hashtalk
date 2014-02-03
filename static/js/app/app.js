65378d06bad07f04fc7aace9844bae995554da61
leops <leopaul3@gmail.com>
2014-02-03 16:46:54 +0100

    Added events, file upload and changed the db URL

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,42 +1,21 @@
 //define(["angular", "socketio", "firebase", "angularfire"], function (angular, io, Firebase) {
-angular.module('hashtalk', ['ngSanitize', 'ngRoute', 'firebase']).value('fbURL', 'https://burning-fire-3933.firebaseio.com/messages').factory('Messages', function ($firebase, fbURL) {
+angular.module('hashtalk', ['ngSanitize', 'ngRoute', 'firebase', 'ui.gravatar']).value('fbURL', 'https://hashtalk.firebaseio.com/messages').factory('Messages', function ($firebase, fbURL) {
 	return $firebase(new Firebase(fbURL));
 }).factory('socket', function ($rootScope) {
-	var socket = io.connect();
-	return {
-		on: function (eventName, callback) {
-			socket.on(eventName, function () {
-				var args = arguments;
-				$rootScope.$apply(function () {
-					callback.apply(socket, args);
-				});
-			});
-		},
-		emit: function (eventName, data, callback) {
-			socket.emit(eventName, data, function () {
-				var args = arguments;
-				$rootScope.$apply(function () {
-					if (callback) {
-						callback.apply(socket, args);
-					}
-				});
-			})
-		}
-	};
+	return io.connect();
 }).controller('HTCtrl', function ($scope, $route, $routeParams, $location, Messages, socket, $sce) {
-	if (localStorage['pseudo'])
+	if (localStorage['pseudo']) {
 		$scope.pseudo = localStorage['pseudo'];
+		socket.emit('pseudo', $scope.pseudo);
+	}
 	$scope.messages = Messages;
-	$scope.search = {};
+	$scope.search = {
+		hashtag: '',
+		pseudo: ''
+	};
 	$scope.submit = function () {
 		if ($scope.postForm.$valid) {
 			socket.emit('message', {
-				pseudo: this.pseudo,
-				msg: this.msg,
-				hashtag: $scope.search.hashtag
-			});
-			console.log({
-				pseudo: this.pseudo,
 				msg: this.msg,
 				hashtag: $scope.search.hashtag
 			});
@@ -45,17 +24,48 @@ angular.module('hashtalk', ['ngSanitize', 'ngRoute', 'firebase']).value('fbURL',
 			console.error("Invalid", $scope.postForm.$valid);
 	};
 	$scope.setPseudo = function () {
-		localStorage['pseudo'] = $scope.pseudo;
+		if (localStorage['pseudo'] != $scope.pseudo) {
+			socket.emit('pseudo', $scope.pseudo);
+			localStorage['pseudo'] = $scope.pseudo;
+		}
 	};
 	$scope.format = function (msg) {
-		if (typeof msg === 'string')
-			return $sce.trustAsHtml(msg.replace(/\#(\S+)/g, function (match, hashtag) {
-				return '<a class="label label-primary" onclick="angular.element(\'body\').scope().$apply(function($scope){$scope.search.hashtag = \'' + hashtag + '\';});" href="#">' + match + '</a>';
-			}).replace(/\@(\S+)/g, function (match, pseudo) {
-				return '<a class="label label-success" onclick="angular.element(\'body\').scope().$apply(function($scope){$scope.search.pseudo = \'' + pseudo + '\';});" href="#">' + match + '</a>';
-			}));
-		else
-			return msg;
+		msg.time = moment(msg.time).lang('fr').fromNow();
+		if (typeof msg.msg === 'string') {
+			if (msg.type == 'message')
+				msg.msg = msg.msg.replace(/\#(\S+)/g, function (match, hashtag) {
+					return '<a class="label label-primary" onclick="angular.element(\'body\').scope().$apply(function($scope){$scope.search.hashtag = \'' + hashtag + '\';});" href="#">' + match + '</a>';
+				}).replace(/\@(\S+)/g, function (match, pseudo) {
+					return '<a class="label label-success" onclick="angular.element(\'body\').scope().$apply(function($scope){$scope.search.pseudo = \'' + pseudo + '\';});" href="#">' + match + '</a>';
+				});
+			else if (msg.type == 'event')
+				msg.msg = '<i>' + msg.msg + '</i>';
+
+			if (msg.type == 'file')
+				msg.msg = $sce.trustAsResourceUrl(msg.msg);
+			else
+				msg.msg = $sce.trustAsHtml(msg.msg);
+		}
+		return msg;
 	};
+	$scope.msgFilter = function (value) {
+		return (value.pseudo == $scope.pseudo) || ((new RegExp('^.*' + $scope.search.hashtag + '.*$', 'gi').test(value.hashtag)) || ($scope.search.hashtag == '')) || ((new RegExp('^.*' + $scope.search.pseudo + '.*$', 'gi').test(value.pseudo)) || ($scope.search.pseudo == ''));
+	};
+	$(document.body).on('dragover', function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.originalEvent.dataTransfer.dropEffect = 'copy';
+		$('#postBox').addClass('drag');
+	}).on('drop', function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		for (var i = 0, f; f = e.originalEvent.dataTransfer.files[i]; i++) {
+			var stream = ss.createStream();
+			ss(socket).emit('upload', stream, {
+				type: f.type
+			});
+			ss.createBlobReadStream(f).pipe(stream);
+		}
+	});
 });
 //});
--- web.js
+++ web.js
@@ -2,16 +2,58 @@ var express = require('express'),
 	app = module.exports = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
+	ss = require('socket.io-stream'),
 	Firebase = require('firebase'),
 	FirebaseTokenGenerator = require("firebase-token-generator"),
 	tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET),
 	token = tokenGenerator.createToken({
 		"isServer": true
-	});
+	}),
+	base64 = require('base64-stream');
 
-app.set('port', process.env.PORT || 5000);
-app.use(express.static(__dirname + '/static'));
-io.set("log level", 0);
+app.configure(function () {
+	app.set('port', process.env.PORT || 5000);
+	app.use(express.compress());
+	app.use(express.timeout());
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/static'));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler());
+});
+
+app.configure('production', function () {
+	app.use(function (err, req, res, next) {
+		console.error(err);
+		next(err);
+	});
+	app.use(function (err, req, res, next) {
+		if (req.xhr) {
+			res.send(500, {
+				error: 'FAIL !'
+			});
+		} else {
+			next(err);
+		}
+	});
+	app.use(function (err, req, res, next) {
+		res.status(500);
+		res.render('error', {
+			error: err
+		});
+	});
+});
+
+io.configure('development', function () {
+	io.set("log level", 1);
+});
+
+io.configure('production', function () {
+	io.set("log level", 0);
+});
 
 function postMessage(msg) {
 	var db = new Firebase(process.env.FIREBASE_URL);
@@ -25,30 +67,87 @@ function postMessage(msg) {
 }
 
 io.sockets.on('connection', function (socket) {
+	ss(socket).on('upload', function (stream, data) {
+		var buffer = '';
+		stream.pipe(base64.encode()).on('data', function (chunk) {
+			buffer += chunk;
+		}).on('end', function () {
+			socket.get('pseudo', function (err, pseudo) {
+				postMessage({
+					type: 'file',
+					pseudo: [pseudo],
+					time: new Date().getTime(),
+					hashtag: [''],
+					msg: 'data:' + data.type + ';base64,' + buffer
+				});
+			});
+		});
+	});
 	socket.on('message', function (data) {
 		if (data.hashtag)
 			data.hashtag = [data.hashtag];
 		else
 			data.hashtag = [''];
 
-		if (data.pseudo)
-			data.pseudo = [data.pseudo];
-		else {
-			console.error("No pseudo", data);
-			return;
-		}
+		socket.get('pseudo', function (err, pseudo) {
+			if (!pseudo || err) {
+				console.error(data, err);
+				return;
+			} else {
+				data.pseudo = [pseudo];
+			}
 
-		data.msg.replace(/\#(\S+)/g, function (match, hashtag) {
-			data.hashtag.push(hashtag);
-			return match;
-		}).replace(/\@(\S+)/g, function (match, pseudo) {
-			data.pseudo.push(pseudo);
-			return match;
+			data.msg.replace(/\#(\S+)/g, function (match, hashtag) {
+				data.hashtag.push(hashtag);
+				return match;
+			}).replace(/\@(\S+)/g, function (match, pseudo) {
+				data.pseudo.push(pseudo);
+				return match;
+			});
+
+			data.type = 'message';
+			data.time = new Date().getTime();
+			console.log(data);
+			postMessage(data);
 		});
-
-		console.log(data);
-		postMessage(data);
-	})
+	});
+	socket.on('pseudo', function (data) {
+		socket.get('pseudo', function (err, old) {
+			socket.set('pseudo', data, function () {
+				msg = {
+					type: 'event',
+					time: new Date().getTime(),
+					hashtag: ['']
+				};
+				if (old) {
+					msg.pseudo = [old];
+					msg.msg = 'à changé de pseudo : ' + data;
+				} else {
+					msg.pseudo = [data];
+					msg.msg = 'viens de se connecter';
+				}
+				console.log(msg);
+				postMessage(msg);
+			});
+		});
+	});
+	socket.on('disconnect', function () {
+		socket.get('pseudo', function (err, pseudo) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			msg = {
+				type: 'event',
+				pseudo: [pseudo],
+				time: new Date().getTime(),
+				msg: 's\'est déconnecté ',
+				hashtag: ['']
+			};
+			console.log(msg);
+			postMessage(msg);
+		});
+	});
 });
 
 server.listen(app.get('port'), function () {
